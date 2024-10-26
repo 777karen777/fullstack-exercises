72e755b532e559abcfc9c13c15a12c23a7d5bedf
Karen <karen.nikoghosyan.her@gmail.com>
2024-10-26 14:07:49 +0400

    1.7: unicafe step 2

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -15,18 +15,28 @@ const Button = ({onClickHandler, text}) => {
 }
 
 const Statistics = ({feedback}) => {
+  const all = feedback.good + feedback.neutral + feedback.bad
+  const positive = feedback.good / (all / 100)
+  const average = (feedback.good - feedback.bad) / all
   return (
     <div>
       <div>good {feedback.good}</div>
       <div>neutral {feedback.neutral}</div>
       <div>bad {feedback.bad}</div>
+      <div>all {all}</div>
+      <div>average {average}</div>
+      <div>positive {positive} %</div>
     </div>
   )
 }
 
 const App = () => {
 
-  const [feedback, setFeedback] = useState({good: 0, neutral: 0, bad: 0})
+  const [feedback, setFeedback] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0
+  })
 
   const onClickGood = () => {
     return (
