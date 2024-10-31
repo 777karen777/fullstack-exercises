9d294171aa4b64087b64481e77c4b2cc939b0b6a
Karen <karen.nikoghosyan.her@gmail.com>
2024-10-31 06:48:04 +0400

    2.8: The Phonebook Step 3

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -4,12 +4,17 @@ import Person from './components/Person'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-1234567'}
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
-  const handleNameCange = (event) => {
+  const handleNameChange = (event) => {
     setNewName(event.target.value)
+  }
+  
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
   }
 
   const addPerson = (event) => {
@@ -20,10 +25,11 @@ const App = () => {
       alert(`${newName} is already added to phonebook`)
     }
     else {
-      const newPerson = {name: newName}
+      const newPerson = {name: newName, number: newNumber}
       const newPersons = persons.concat(newPerson)
       setPersons(newPersons)
       setNewName('')
+      setNewNumber('')
     }    
   }
 
@@ -32,7 +38,10 @@ const App = () => {
       <h2>Phonebook</h2>
       <form  onSubmit={addPerson}>
         <div>
-          name: <input value={newName} onChange={handleNameCange}/>
+          name: <input value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange}/>
         </div>
         <div>
           <button type="submit">add</button>
